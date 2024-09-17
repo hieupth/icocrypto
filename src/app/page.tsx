39dd2ce212a90cf05150cf886ade482ff414900b
ThinhPhoenix <laithinh1310@gmail.com>
2024-09-17 13:11:05 +0700

    fix: use client page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Ikon from "@/components/elements/element-ikon";
 import Modals from "@/components/elements/element-modals";
 import PictureModals from "@/components/elements/element-picture-modals";
