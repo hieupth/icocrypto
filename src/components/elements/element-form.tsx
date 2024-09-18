6a2b3c31469898ebefe3f3d1ceb7e7be4f0be08e
MinhTrung187 <nguyenminhtrung18072004@gmail.com>
2024-09-18 11:42:01 +0700

    update form

--- src/components/elements/element-form.tsx
+++ src/components/elements/element-form.tsx
@@ -1,218 +1,120 @@
-import React from "react"
+"use client";
 
-const Input_bordered: React.FC = () => {
-    return (
+import React from "react";
 
-        <div className="field-item">
-            <label className="field-label">Input Element</label>
-            <div className="field-wrap">
-                <input name="inputname1" type="text" className="input-bordered" id="input-text-2" required />
-            </div>
-            <p>Use <code>.input-bordered</code> class with input element to make it bordered.</p>
+// Input Bordered Component
+interface InputProps {
+    name: string;
+    label: string;
+}
+
+const InputBordered: React.FC<InputProps> = ({ name, label }) => (
+    <div className="field-item">
+        <label className="field-label">{label}</label>
+        <div className="field-wrap">
+            <input name={name} type="text" className="input-bordered" required />
         </div>
-    );
+        <p>Use <code>.input-bordered</code> class with input element to make it bordered.</p>
+    </div>
+);
+
+// Form Select Component
+interface SelectProps {
+    name: string;
+    label: string;
+    options: { value: string; label: string }[];
 }
 
-const Form_Select: React.FC = () => {
-    return (
-        <div className="field-item">
-            <label className="field-label">Select Element</label>
-            <div className="field-wrap">
-                <select name="option2" className="select2-container select-bordered select2-selection" data-select2-theme="bordered" required>
-                    <option value="">Please select</option>
-                    <option value="option_a">Option A</option>
-                    <option value="option_b">Option B</option>
-                    <option value="option_c">Option C</option>
-                    <option value="option_d">Option D</option>
-                </select>
-            </div>
-            <p>Use <code>.select</code> to activate select2 jQuery plugin for custom select field style. Use <code>data-select2-theme="bordered"</code> for a bordered style.</p>
+const FormSelect: React.FC<SelectProps> = ({ name, label, options }) => (
+    <div className="field-item">
+        <label className="field-label">{label}</label>
+        <div className="field-wrap">
+            <select name={name} className=".select2-container.select-bordered .select2-selection" data-select2-theme="bordered" required>
+                <option value="">Please select</option>
+                {options.map(option => (
+                    <option key={option.value} value={option.value}>{option.label}</option>
+                ))}
+            </select>
         </div>
-    );
-}
+        <p>Use <code>.select-bordered</code> for bordered select element styling.</p>
+    </div>
+);
 
-const Form_Select_WithSearch: React.FC = () => {
-    return (
-        <div className="field-item">
-            <label className="field-label">Select Element (with search enabled)</label>
-            <div className="field-wrap">
-                <select name="option2.1" className="select select2-accessible error" data-select2-theme="bordered" data-search="on" required data-select2-id="4">
-                    <option value="">Please select</option>
-                    <option value="option_a">Option A</option>
-                    <option value="option_b">Option B</option>
-                    <option value="option_c">Option C</option>
-                    <option value="option_d">Option D</option>
-                </select>
-            </div>
-            <p>Use <code>data-search="on"</code> to enable search for the select2 element.</p>
+// Text Area Component
+const TextArea: React.FC<InputProps> = ({ name, label }) => (
+    <div className="field-item">
+        <label className="field-label">{label}</label>
+        <div className="field-wrap">
+            <textarea name={name} className="input-bordered input-textarea" required></textarea>
         </div>
-    );
-}
+        <p>Use <code>.input-textarea</code> class for textarea.</p>
+    </div>
+);
 
-const Text_area: React.FC = () => {
-    return (
-
-        <div className="field-item">
-            <label className="field-label">Textarea Element</label>
-            <div className="field-wrap">
-                <textarea name="textarea2" className="input-bordered input-textarea" required></textarea>
-            </div>
-            <p>Use <code>.input-textarea</code> with input classes for textarea.</p>
-        </div>
-
-    );
-}
-const Check_box: React.FC = () => {
-    return (
-        <>
-            <ul className="d-flex flex-wrap gutter-30px">
-                <li className="mb-3">
+// Checkbox Component
+const CheckBox: React.FC<{ items: string[] }> = ({ items }) => (
+    <>
+        <ul className="d-flex flex-wrap gutter-30px">
+            {items.map((item, index) => (
+                <li className="mb-3" key={index}>
                     <div className="field-wrap">
-                        <input className="input-checkbox" id="cbi-1" name="cbi[]" type="checkbox" required />
-                        <label htmlFor="cbi-1">CheckBox Item</label>
+                        <input className="input-checkbox" id={`cbi-${index}`} name={`cbi[]`} type="checkbox" required />
+                        <label htmlFor={`cbi-${index}`}>{item}</label>
                     </div>
                 </li>
-                <li className="mb-3">
+            ))}
+        </ul>
+        <p>Use <code>.input-checkbox</code> class for checkbox inputs.</p>
+    </>
+);
+
+// Radio Component
+const Radio: React.FC<{ items: string[] }> = ({ items }) => (
+    <>
+        <ul className="d-flex flex-wrap gutter-30px">
+            {items.map((item, index) => (
+                <li className="mb-3" key={index}>
                     <div className="field-wrap">
-                        <input className="input-checkbox" id="cbi-2" name="cbi[]" type="checkbox" required />
-                        <label htmlFor="cbi-2">CheckBox Item</label>
+                        <input className="input-radio" id={`rdi-${index}`} name="rdi" type="radio" required />
+                        <label htmlFor={`rdi-${index}`}>{item}</label>
                     </div>
                 </li>
-                <li className="mb-3">
+            ))}
+        </ul>
+        <p>Use <code>.input-radio</code> class for radio inputs.</p>
+    </>
+);
+
+// Switch Component
+const Switch: React.FC<{ items: string[] }> = ({ items }) => (
+    <>
+        <ul className="d-flex flex-wrap gutter-30px">
+            {items.map((item, index) => (
+                <li className="mb-3" key={index}>
                     <div className="field-wrap">
-                        <input className="input-checkbox" id="cbi-3" name="cbi[]" type="checkbox" required />
-                        <label htmlFor="cbi-3">CheckBox Item</label>
+                        <input className="input-switch" id={`switch-${index}`} name={`switch[]`} type="checkbox" required />
+                        <label htmlFor={`switch-${index}`}>{item}</label>
                     </div>
                 </li>
-            </ul>
-            <p>Use <code>.input-checkbox</code> class for checkbox inputs.</p>
-        </>
+            ))}
+        </ul>
+        <p>Use <code>.input-switch</code> for switch inputs.</p>
+    </>
+);
 
-    );
-}
-
-const Radio: React.FC = () => {
-    return (
-        <>
-            <ul className="d-flex flex-wrap gutter-30px">
-                <li className="mb-3">
-                    <div className="field-wrap">
-                        <input className="input-radio" id="rdi-1" name="rdi" type="radio" required />
-                        <label htmlFor="rdi-1">Radio Item</label>
-                    </div>
-                </li>
-                <li className="mb-3">
-                    <div className="field-wrap">
-                        <input className="input-radio" id="rdi-2" name="rdi" type="radio" required />
-                        <label htmlFor="rdi-2">Radio Item</label>
-                    </div>
-                </li>
-                <li className="mb-3">
-                    <div className="field-wrap">
-                        <input className="input-radio" id="rdi-3" name="rdi" type="radio" required />
-                        <label htmlFor="rdi-3">Radio Item</label>
-                    </div>
-                </li>
-            </ul>
-            <p>Use <code>.input-radio</code> class for radio inputs.</p>
-        </>
-
-
-    );
-}
-
-const Switch: React.FC = () => {
-    return (
-        <>
-            <ul className="d-flex flex-wrap gutter-30px">
-                <li className="mb-3">
-                    <div className="field-wrap">
-                        <input className="input-switch" id="switch-1" name="switch[]" type="checkbox" required />
-                        <label htmlFor="switch-1">Switch Item</label>
-                    </div>
-                </li>
-                <li className="mb-3">
-                    <div className="field-wrap">
-                        <input className="input-switch" id="switch-2" name="switch[]" type="checkbox" required />
-                        <label htmlFor="switch-2">Switch Item</label>
-                    </div>
-                </li>
-                <li className="mb-3">
-                    <div className="field-wrap">
-                        <input className="input-switch" id="switch-3" name="switch[]" type="checkbox" required />
-                        <label htmlFor="switch-3">Switch Item</label>
-                    </div>
-                </li>
-            </ul>
-            <p>Use <code>.input-switch</code> for switch inputs.</p>
-
-        </>
-
-    );
-}
-
-const Input_line: React.FC = () => {
-    return (
-
-        <div className="field-item">
-            <div className="field-wrap input-focused">
-                <input name="inputname2" type="text" className="input-line" id="input-text-1" required />
-                <label className="field-label field-label-line">Input Element</label>
-            </div>
-            <p>Use <code>.input-line</code> class for line input. Add <code>.field-label .field-label-line</code> for label animation.</p>
-        </div>
-
-    );
-}
-
-const Form_line: React.FC = () => {
-    return (
-
-        <div className="field-item">
-            <div className="field-wrap">
-                <select name="option1" className="select" data-select2-theme="line" required>
-                    <option value="">Please select</option>
-                    <option value="option_a">Option A</option>
-                    <option value="option_b">Option B</option>
-                    <option value="option_c">Option C</option>
-                    <option value="option_d">Option D</option>
-                </select>
-            </div>
-            <p>Use <code>.select</code> to activate select2 plugin for custom style, and <code>data-select2-theme="line"</code> for line style.</p>
-        </div>
-
-    );
-}
-
-const Text_area_input: React.FC = () => {
-    return (
-
-        <div className="field-item">
-            <div className="field-wrap input-focused">
-                <textarea name="textarea1" id="textarea" className="input-line input-textarea" required></textarea>
-                <label className="field-label field-label-line">Textarea Element</label>
-            </div>
-            <p>Use <code>.input-textarea</code> for textarea input.</p>
-        </div>
-
-    );
-}
-
-const Banner: React.FC = () => {
-    return (
-
-        <div className="header-banner bg-theme-grad">
-            <div className="nk-banner">
-                <div className="banner banner-page">
-                    <div className="banner-wrap">
-                        <div className="container">
-                            <div className="row justify-content-center">
-                                <div className="col-xl-6 col-lg-9">
-                                    <div className="banner-caption cpn tc-light text-center">
-                                        <div className="cpn-head">
-                                            <h2 className="title ttu">Form</h2>
-                                            <p>We designed a brand-new cool design and lots of features, the latest version of the template supports advanced block base scenarios, and more.</p>
-                                        </div>
+// Banner Component
+const Banner: React.FC = () => (
+    <div className="header-banner bg-theme-grad">
+        <div className="nk-banner">
+            <div className="banner banner-page">
+                <div className="banner-wrap">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-xl-6 col-lg-9">
+                                <div className="banner-caption cpn tc-light text-center">
+                                    <div className="cpn-head">
+                                        <h2 className="title ttu">Form</h2>
+                                        <p>We designed a brand-new cool design and lots of features, the latest version of the template supports advanced block base scenarios, and more.</p>
                                     </div>
                                 </div>
                             </div>
@@ -222,28 +124,36 @@ const Banner: React.FC = () => {
             </div>
             <div className="nk-ovm shape-a-sm"></div>
         </div>
-    );
-}
+    </div>
+);
 
+// Main Form Component
+const Form: React.FC = () => {
+    const selectOptions = [
+        { value: "option_a", label: "Option A" },
+        { value: "option_b", label: "Option B" },
+        { value: "option_c", label: "Option C" },
+        { value: "option_d", label: "Option D" }
+    ];
 
+    const checkBoxItems = ["CheckBox Item 1", "CheckBox Item 2", "CheckBox Item 3"];
+    const radioItems = ["Radio Item 1", "Radio Item 2", "Radio Item 3"];
+    const switchItems = ["Switch Item 1", "Switch Item 2", "Switch Item 3"];
 
-
-export default function Form() {
     return (
         <>
-            <Banner/>
+            {/* <Banner /> */}
             <main className="nk-pages">
                 <section className="section">
                     <div className="container">
-                        <h3 className="title title-md ">Form Element Bordered</h3>
-                        <form action="#" className="form-validate">
-                            <Input_bordered />
-                            <Form_Select />
-                            <Form_Select_WithSearch />
-                            <Text_area />
-                            <Check_box />
-                            <Radio />
-                            <Switch />
+                        <h3 className="title title-md">Form Element Bordered</h3>
+                        <form className="form-validate">
+                            <InputBordered name="inputname1" label="Input Element" />
+                            <FormSelect name="option2" label="Select Element" options={selectOptions} />
+                            <TextArea name="textarea2" label="Textarea Element" />
+                            <CheckBox items={checkBoxItems} />
+                            <Radio items={radioItems} />
+                            <Switch items={switchItems} />
                             <button className="btn btn-grad">Verify</button>
                         </form>
                     </div>
@@ -252,21 +162,17 @@ export default function Form() {
                 <section className="section">
                     <div className="container">
                         <h3 className="title title-md">Form Element Line</h3>
-                        <form action="#" className="form-validate">
-                            <Input_line />
-                            <Form_line />
-                            <Text_area_input />
-
+                        <form className="form-validate">
+                            <InputBordered name="inputname2" label="Input Element (Line)" />
+                            <FormSelect name="option1" label="Select Element (Line)" options={selectOptions} />
+                            <TextArea name="textarea1" label="Textarea Element (Line)" />
                             <button className="btn btn-grad">Verify</button>
                         </form>
                     </div>
                 </section>
             </main>
-
-
         </>
+    );
+};
 
-
-    )
-
-}
+export default Form;
