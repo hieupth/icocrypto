a17ed67c7d8902945ea24072bab3e6975f260c1e
Minh Hưng <ngominhhung208@gmail.com>
2024-09-25 12:44:53 +0700

    Chore: fix tab and write block FAQ

--- src/components/blocks/block-FAQ.tsx
+++ src/components/blocks/block-FAQ.tsx
@@ -1,47 +1,72 @@
-import { TabRow } from "../elements/element-tab";
+import { Accordion, AccordionItem } from "../elements/element-accordion";
+import { TabItem, TabS1, TabS2, TabS3, TabS4 } from "../elements/element-tab";
+import { getColorStyleClass, ColorStyle, Stylable } from "@/utils/colorclass";
 
-export const BlockFAQDefault: React.FC<{ children: React.ReactNode }> = ({
-    children,
+interface FAQItem {
+    title: string;
+    content: string;
+}
+
+interface TabContent {
+    id: string;
+    title: string;
+    faqs: FAQItem[];
+}
+
+interface BlockFAQProps {
+    tabs: TabContent[];
+    color?: ColorStyle;
+    style?: Stylable;
+    dark?: boolean;
+    variant: "S1" | "S2" | "S3" | "S4";
+}
+
+export const BlockFAQ: React.FC<BlockFAQProps> = ({
+    tabs,
+    color = "default", // Default color if not provided
+    style = "",
+    dark = false, // Default dark mode to false
+    variant = "S1",
 }) => {
+    const darkClass = dark ? "dark" : "";
+    const styleClass = style ? style : "";
+    const colorClass = color ? color : "default";
+    const TabComponent =
+        variant === "S1"
+            ? TabS1
+            : variant === "S2"
+            ? TabS2
+            : variant === "S3"
+            ? TabS3
+            : variant === "S4"
+            ? TabS4
+            : TabS1; // Add additional logic for S3 and S4 if needed
+    const AccodionComponent =
+        variant === "S1"
+            ? "S1"
+            : variant === "S2"
+            ? "S2"
+            : variant === "S3"
+            ? "S3"
+            : variant === "S4"
+            ? "S4"
+            : "S1";
+
     return (
-        <section className="section bg-white">
-            <TabRow>{children}</TabRow>
-        </section>
-    );
-};
-export const BlockFAQLight: React.FC<{ children: React.ReactNode }> = ({
-    children,
-}) => {
-    return (
-        <section className="section bg-light">
-            <TabRow>{children}</TabRow>
-        </section>
-    );
-};
-export const BlockFAQTheme: React.FC<{ children: React.ReactNode }> = ({
-    children,
-}) => {
-    return (
-        <section className="section bg-theme">
-            <TabRow>{children}</TabRow>
-        </section>
-    );
-};
-export const BlockFAQThemeDark: React.FC<{ children: React.ReactNode }> = ({
-    children,
-}) => {
-    return (
-        <section className="section bg-theme-dark">
-            <TabRow>{children}</TabRow>
-        </section>
-    );
-};
-export const BlockFAQBgimage: React.FC<{ children: React.ReactNode }> = ({
-    children,
-}) => {
-    return (
-        <section className="section">
-            <TabRow>{children}</TabRow>
+        <section className={`section ${styleClass} ${colorClass} ${darkClass}`}>
+            <TabComponent>
+                {tabs.map((tab) => (
+                    <TabItem key={tab.id} id={tab.id} title={tab.title}>
+                        <Accordion variant={AccodionComponent}>
+                            {tab.faqs.map((faq, index) => (
+                                <AccordionItem key={index} title={faq.title}>
+                                    <p>{faq.content}</p>
+                                </AccordionItem>
+                            ))}
+                        </Accordion>
+                    </TabItem>
+                ))}
+            </TabComponent>
         </section>
     );
 };
--- src/components/elements/element-tab.tsx
+++ src/components/elements/element-tab.tsx
@@ -14,7 +14,7 @@ export const TabItem: React.FC<TabItemProps> = ({ title, children }) => {
 };
 
 // Define the TabRow component (handles TabItem)
-export const TabRow: React.FC<{
+export const TabS1: React.FC<{
     children: React.ReactNode; // This allows both single and multiple children
 }> = ({ children }) => {
     const elements = React.Children.toArray(
@@ -23,46 +23,52 @@ export const TabRow: React.FC<{
     const [activeTab, setActiveTab] = useState<string>(elements[0]?.props.id);
 
     return (
-        <div className="row">
-            {/* Tab Navigation */}
-            <div className="col-md-4 mb-4 mb-lg-0">
-                <ul className="nav tab-nav tab-nav-vr tab-nav-bdr me-lg-3">
-                    {elements.map((element) => (
-                        <li key={element.props.id}>
-                            <a
-                                className={
-                                    activeTab === element.props.id
-                                        ? "active"
-                                        : ""
-                                }
-                                onClick={() => setActiveTab(element.props.id)}
-                                href={`#${element.props.id}`}
-                                data-bs-toggle="tab"
-                            >
-                                <em className="fas fa-caret-right"></em>
-                                {element.props.title}
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+        <div className="row justify-content-center">
+            <div className="col-lg-10">
+                <div className="row">
+                    {/* Tab Navigation */}
+                    <div className="col-md-4 mb-4 mb-lg-0">
+                        <ul className="nav tab-nav tab-nav-vr tab-nav-bdr me-lg-3">
+                            {elements.map((element) => (
+                                <li key={element.props.id}>
+                                    <a
+                                        className={
+                                            activeTab === element.props.id
+                                                ? "active"
+                                                : ""
+                                        }
+                                        onClick={() =>
+                                            setActiveTab(element.props.id)
+                                        }
+                                        href={`#${element.props.id}`}
+                                        data-bs-toggle="tab"
+                                    >
+                                        <em className="fas fa-caret-right"></em>
+                                        {element.props.title}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
 
-            {/* Tab Content */}
-            <div className="col-md-8">
-                <div className="tab-content">
-                    {elements.map((element) => (
-                        <div
-                            key={element.props.id}
-                            className={`tab-pane fade ${
-                                activeTab === element.props.id
-                                    ? "show active"
-                                    : ""
-                            }`}
-                            id={element.props.id}
-                        >
-                            {element.props.children}
+                    {/* Tab Content */}
+                    <div className="col-md-8">
+                        <div className="tab-content">
+                            {elements.map((element) => (
+                                <div
+                                    key={element.props.id}
+                                    className={`tab-pane fade ${
+                                        activeTab === element.props.id
+                                            ? "show active"
+                                            : ""
+                                    }`}
+                                    id={element.props.id}
+                                >
+                                    {element.props.children}
+                                </div>
+                            ))}
                         </div>
-                    ))}
+                    </div>
                 </div>
             </div>
         </div>
@@ -70,55 +76,179 @@ export const TabRow: React.FC<{
 };
 
 // Define TabLine component
-export const TabLine: React.FC<{
+export const TabS2: React.FC<{
     children: React.ReactElement<TabItemProps>[]; // Updated to use TabItemProps
 }> = ({ children }) => {
     const [activeTab, setActiveTab] = useState<string>(children[0]?.props.id);
 
     return (
-        <div className="col-lg-10">
-            {/* Tab Navigation */}
-            <ul className="nav tab-nav tab-nav-line tab-nav-center">
-                {React.Children.map(children, (child) => {
-                    const element = child as React.ReactElement<TabItemProps>;
-                    return element.props ? (
-                        <li key={element.props.id}>
-                            <a
-                                className={
-                                    activeTab === element.props.id
-                                        ? "active"
-                                        : ""
-                                }
-                                onClick={() => setActiveTab(element.props.id)}
-                                href={`#${element.props.id}`}
-                                data-bs-toggle="tab"
-                            >
-                                <em className="fas fa-caret-right"></em>
-                                {element.props.title}
-                            </a>
-                        </li>
-                    ) : null;
-                })}
-            </ul>
+        <div className="row justify-content-center">
+            <div className="col-lg-10">
+                <ul className="nav tab-nav tab-nav-btn">
+                    {React.Children.map(children, (child) => {
+                        const element =
+                            child as React.ReactElement<TabItemProps>;
+                        return element.props ? (
+                            <li key={element.props.id}>
+                                <a
+                                    className={
+                                        activeTab === element.props.id
+                                            ? "active"
+                                            : ""
+                                    }
+                                    onClick={() =>
+                                        setActiveTab(element.props.id)
+                                    }
+                                    href={`#${element.props.id}`}
+                                    data-bs-toggle="tab"
+                                >
+                                    <em className="fas fa-caret-right"></em>
+                                    {element.props.title}
+                                </a>
+                            </li>
+                        ) : null;
+                    })}
+                </ul>
 
-            {/* Tab Content */}
-            <div className="tab-content">
-                {React.Children.map(children, (child) => {
-                    const element = child as React.ReactElement<TabItemProps>;
-                    return element.props ? (
-                        <div
-                            key={element.props.id}
-                            className={`tab-pane fade ${
-                                activeTab === element.props.id
-                                    ? "show active"
-                                    : ""
-                            }`}
-                            id={element.props.id}
-                        >
-                            {element.props.children}
-                        </div>
-                    ) : null;
-                })}
+                {/* Tab Content */}
+                <div className="tab-content">
+                    {React.Children.map(children, (child) => {
+                        const element =
+                            child as React.ReactElement<TabItemProps>;
+                        return element.props ? (
+                            <div
+                                key={element.props.id}
+                                className={`tab-pane fade ${
+                                    activeTab === element.props.id
+                                        ? "show active"
+                                        : ""
+                                }`}
+                                id={element.props.id}
+                            >
+                                {element.props.children}
+                            </div>
+                        ) : null;
+                    })}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+// Define TabLine component
+export const TabS3: React.FC<{
+    children: React.ReactElement<TabItemProps>[]; // Updated to use TabItemProps
+}> = ({ children }) => {
+    const [activeTab, setActiveTab] = useState<string>(children[0]?.props.id);
+
+    return (
+        <div className="row justify-content-center">
+            <div className="col-lg-10">
+                <ul className="nav tab-nav tab-nav-s2 tab-nav-center">
+                    {React.Children.map(children, (child) => {
+                        const element =
+                            child as React.ReactElement<TabItemProps>;
+                        return element.props ? (
+                            <li key={element.props.id}>
+                                <a
+                                    className={
+                                        activeTab === element.props.id
+                                            ? "active"
+                                            : ""
+                                    }
+                                    onClick={() =>
+                                        setActiveTab(element.props.id)
+                                    }
+                                    href={`#${element.props.id}`}
+                                    data-bs-toggle="tab"
+                                >
+                                    <em className="fas fa-caret-right"></em>
+                                    {element.props.title}
+                                </a>
+                            </li>
+                        ) : null;
+                    })}
+                </ul>
+
+                {/* Tab Content */}
+                <div className="tab-content">
+                    {React.Children.map(children, (child) => {
+                        const element =
+                            child as React.ReactElement<TabItemProps>;
+                        return element.props ? (
+                            <div
+                                key={element.props.id}
+                                className={`tab-pane fade ${
+                                    activeTab === element.props.id
+                                        ? "show active"
+                                        : ""
+                                }`}
+                                id={element.props.id}
+                            >
+                                {element.props.children}
+                            </div>
+                        ) : null;
+                    })}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export const TabS4: React.FC<{
+    children: React.ReactElement<TabItemProps>[]; // Updated to use TabItemProps
+}> = ({ children }) => {
+    const [activeTab, setActiveTab] = useState<string>(children[0]?.props.id);
+
+    return (
+        <div className="row justify-content-center">
+            <div className="col-lg-10">
+                <ul className="nav tab-nav tab-nav-line tab-nav-center">
+                    {React.Children.map(children, (child) => {
+                        const element =
+                            child as React.ReactElement<TabItemProps>;
+                        return element.props ? (
+                            <li key={element.props.id}>
+                                <a
+                                    className={
+                                        activeTab === element.props.id
+                                            ? "active"
+                                            : ""
+                                    }
+                                    onClick={() =>
+                                        setActiveTab(element.props.id)
+                                    }
+                                    href={`#${element.props.id}`}
+                                    data-bs-toggle="tab"
+                                >
+                                    <em className="fas fa-caret-right"></em>
+                                    {element.props.title}
+                                </a>
+                            </li>
+                        ) : null;
+                    })}
+                </ul>
+
+                {/* Tab Content */}
+                <div className="tab-content">
+                    {React.Children.map(children, (child) => {
+                        const element =
+                            child as React.ReactElement<TabItemProps>;
+                        return element.props ? (
+                            <div
+                                key={element.props.id}
+                                className={`tab-pane fade ${
+                                    activeTab === element.props.id
+                                        ? "show active"
+                                        : ""
+                                }`}
+                                id={element.props.id}
+                            >
+                                {element.props.children}
+                            </div>
+                        ) : null;
+                    })}
+                </div>
             </div>
         </div>
     );
