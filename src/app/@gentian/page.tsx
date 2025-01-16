575cf8558ffb0e1d649275b66f322e1064ef0f38
HDNghia <huynhdangnghia68@gmail.com>
2025-01-16 15:14:23 +0700

    add core team and blockbanner

--- src/app/@gentian/page.tsx
+++ src/app/@gentian/page.tsx
@@ -1,4 +1,18 @@
+import BlockBanner from "@/components/blocks/block-banner";
+import BlockCoreTeam from "@/components/blocks/block-core-team";
 
 export default function Page() {
-  return (<></>)
+    return (
+        <>
+            <BlockBanner variant="S1" />
+            <section className="section section-team bg-theme-dark tc-light pt-lg-8">
+                <div className="container">
+                    <div className="section-head text-center wide-auto">
+                        <h2 className="title title-lg title-dark ">Core Team</h2>
+                    </div>
+                    <BlockCoreTeam />
+                </div>
+            </section>
+        </>
+    );
 }
